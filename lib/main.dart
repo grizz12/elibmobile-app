2b1818a7ad946841c89fe89362b975c5ab3a00f7
grizz12 <raka_088@smkassalaambandung.sch.id>
2022-09-13 19:40:24 +0700

    elibmobile-project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:elibmobile/screens/bottom_nav_bar.dart';
-import 'package:elibmobile/screens/home/home_page.dart';
-import 'package:elibmobile/screens/home/pages/book_details.dart';
+import 'package:elibmobile/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,36 +6,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-   @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: HomePage(),
-      );
-    }
-}
-
-class Splash extends StatelessWidget {
-  const Splash({
-    Key? key,
-    required this.context,
-  }) : super(key: key);
-
-  final BuildContext context;
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ElibMobile',
+      home: SplashPage(),
       debugShowCheckedModeBanner: false,
-      routes: {
-        BottomNavBar.nameRoute: (context) => BottomNavBar(),
-        HomePage.nameRoute: (context) => HomePage(),
-        BookDetail.nameRoute: (context) => BookDetail(),
-      },
+      
     );
-
-    // ignore: dead_code
   }
 }
